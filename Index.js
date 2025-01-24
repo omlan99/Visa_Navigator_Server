e14f1f5a040ae35ba0c51c1f8b28a2079864c68f
omlan99 <omlan10616@gmail.com>
2025-01-24 21:50:55 +0600

    added application api

--- Index.js
+++ Index.js
@@ -28,14 +28,17 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB !");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB !");
 
     // auth related api 
 
     const visaData = client.db('VisaData').collection('AllVisa')
+    
+    const visaApplication = client.db('VisaData').collection('Applications')
+    
     app.get('/', async (req,res)=>{
       const limit = parseInt(req.query.limit) || 100;
         const cursor = visaData.find().limit(limit);
@@ -46,7 +49,6 @@ async function run() {
       const visaId = req.params.id
       const query = {_id : new ObjectId(visaId)}
       const result =await visaData.findOne(query)
-      console.log(result)
       res.send(result)
     })
     app.get('/type', async(req, res) =>{
@@ -75,9 +77,9 @@ async function run() {
 
     
     app.get('/myVisa' , async (req,res) => {
-      const email = req.query.email;
-      const query = {email : email};
-      const result = await visaData.find(query).toArray()
+      // const email = req.query.email;
+      // const query = {email : email};
+      const result = await visaData.find().toArray()
       res.send(result)
     })
 
@@ -91,10 +93,31 @@ async function run() {
       const updateId = req.params.id;
       const updateData = req.body;
       const result = await visaData.updateOne({_id : new ObjectId(updateId)}, { $set: updateData })
-      console.log(result)
       res.send(result)
     })
 
+    // application api
+    app.get('/application' , async(req,res) =>{
+      const email = req.query.email
+      const query = {email : email}
+      if(email){  
+          const result = await visaApplication.find(query).toArray()
+          return res.send(result)
+      } 
+      const result =  await visaApplication.find().toArray()
+      res.send(result)
+
+    }) 
+    app.post('/application', async(req, res) =>{
+      const application = req.body
+      const result = await visaApplication.insertOne(application)
+      res.send(result)
+    })
+    app.delete('/application/:id' , async(req,res) =>{
+       const id = req.params.id
+       const result = await visaApplication.deleteOne({_id : new ObjectId(id)})
+       res.send(result)
+    })
 
   } finally {
     // Ensures that the client will close when you finish/error
